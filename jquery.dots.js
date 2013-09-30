998613ad4b48876ee8bceff01af02c414ec26212
Edgar Perez <edgar.png@gmail.com>
2013-09-30 11:59:32 -0600

    Fix indentation
    Replaced tabs with 2 spaces

--- jquery.dots.js
+++ jquery.dots.js
@@ -12,41 +12,41 @@
  *     $('#my-submit').stopDots({text: 'Saved!'});
  */
 (function($){
-	$.fn.stopDots = function(options){
-		var opts = $.extend({}, options);
-		
-		return $(this).each(function(){
-			var $this = $(this);
-			var data = $this.data('dots') || {};
-			$this.val(opts.text || data.value || $this.val());
-			window.clearInterval(data.interval);
-		});
-	};
-	
-	$.fn.startDots = function(options){
-		var defaults = {interval: 300};
-		var opts = $.extend(defaults, options);
-		
-		return $(this).each(function(){
-			var $this = $(this).stopDots();
-			var data = {};
-			data.value = $this.val();
-			
-			if(opts.text){
-				$this.val(opts.text);
-			}			
-			data.interval = window.setInterval(function() {
-				var value = $this.val();
-				var dotsMatch = value.match(/\.+/);
-				if(dotsMatch && dotsMatch[0].length > 3){
-					$this.val(value.replace(/\.+/,""));
-				}
-				else{
-					$this.val(value+".");
-				}
-			}, opts.interval);
-		
-			$this.data('dots', data);
-		});
-	};
+  $.fn.stopDots = function(options){
+    var opts = $.extend({}, options);
+    
+    return $(this).each(function(){
+      var $this = $(this);
+      var data = $this.data('dots') || {};
+      $this.val(opts.text || data.value || $this.val());
+      window.clearInterval(data.interval);
+    });
+  };
+  
+  $.fn.startDots = function(options){
+    var defaults = {interval: 300};
+    var opts = $.extend(defaults, options);
+    
+    return $(this).each(function(){
+      var $this = $(this).stopDots();
+      var data = {};
+      data.value = $this.val();
+      
+      if(opts.text){
+        $this.val(opts.text);
+      }
+      data.interval = window.setInterval(function() {
+        var value = $this.val();
+        var dotsMatch = value.match(/\.+/);
+        if(dotsMatch && dotsMatch[0].length > 3){
+          $this.val(value.replace(/\.+/,""));
+        }
+        else{
+          $this.val(value+".");
+        }
+      }, opts.interval);
+    
+      $this.data('dots', data);
+    });
+  };
 }(jQuery));
